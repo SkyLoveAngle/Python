cefcc0da4dea700811c3b195b6b029cf2417f136
Qin-SKY <1920909528@qq.com>
2020-10-23 09:10:16 +0800

    Python-code

---
+++ mypy01.py
@@ -0,0 +1,20 @@
+import turtle
+
+'''
+for i in range(5):
+    turtle.forward(300)
+    turtle.right(144)
+
+
+turtle.width(10)
+turtle.color("red")
+turtle.circle(50)
+
+
+turtle.penup()
+turtle.goto(120,0)
+turtle.pendown()
+turtle.width(10)
+turtle.color("red")
+turtle.circle(50)
+'''
---
+++ mypy02.py
@@ -0,0 +1,24 @@
+"""import turtle
+t = turtle.Pen()
+for x in range(360):
+    t.forward(x)
+    t.left(49)
+    
+#这是一条注释
+'''
+1.这也是一个注释
+2,这是安徽省第十四
+3，搭建开发搭建开发的地方
+'''
+"""
+a="abc\rdefg\bhijklmn"
+print(a)
+
+b="aaa0""ccccc"
+print(b)
+
+c = "to be or not to be"[::-1]
+print(c)
+
+d="sxtsxtsxtsxt"[0:11:3]
+print(d)
